a98c0f66e4e6e8e7a31c43c14340e946dff4016b
Marvellous Amasi <102649743+Marveeamasi@users.noreply.github.com>
2025-04-08 16:01:14 +0100

    Update UtilsCard.jsx

--- components/UtilsCard.jsx
+++ components/UtilsCard.jsx
@@ -2,19 +2,23 @@
 
 import Image from "next/image";
 
-export default function UtilsCard() {
+export default function UtilsCard({bg, pos, setShowEdit}) {
+
+   const none = () => {
+      console.log('no function assigned yet!')
+   }
 
   return (
-    <div className="w-[282px] h-[150px] rounded-[8px] bg-[#140926] flex flex-col justify-center items-center p-3 gap-1 z-[11111] absolute bottom-[-160px]">
-     <div className="bg-[#9413E6] p-2 w-[262px] h-[40px] rounded-[4px] flex justify-between items-center cursor-pointer">
+    <div className={`w-[282px] h-[150px] rounded-[8px] bg-[#140926] transition-all duration-300 ease-in-out flex flex-col justify-center items-center p-3 gap-1 z-[11111] absolute ${pos? pos: 'bottom-[-160px]'}`}>
+     <div className={`${bg? bg : 'bg-[#9413e6]'} p-2 w-[262px] hover:animate-pulse h-[40px] rounded-[4px] flex justify-between items-center cursor-pointer`}>
         <div className="font-[500] text-[12.51px]">Delete</div>
         <Image src={'/delete.png'} width={20} height={20} alt='person pics' className='w-[20px] h-[20px]'/>
      </div>
-     <div className="bg-[#9413E6] p-2 w-[262px] h-[40px] rounded-[4px] flex justify-between items-center cursor-pointer">
+     <div onClick={()=> setShowEdit(true) || none} className={`${bg? bg : 'bg-[#9413e6]'} p-2 w-[262px] hover:animate-pulse h-[40px] rounded-[4px] flex justify-between items-center cursor-pointer`}>
         <div className="font-[500] text-[12.51px]">Edit</div>
         <Image src={'/pencil-edit.png'} width={20} height={20} alt='person pics' className='w-[20px] h-[20px]'/>
      </div>
-     <div className="bg-[#9413E6] p-2 w-[262px] h-[40px] rounded-[4px] flex justify-between items-center cursor-pointer">
+     <div className={`${bg? bg : 'bg-[#9413e6]'} p-2 w-[262px] hover:animate-pulse h-[40px] rounded-[4px] flex justify-between items-center cursor-pointer`}>
         <div className="font-[500] text-[12.51px]">Download</div>
         <Image src={'/cloud-download.png'} width={20} height={20} alt='person pics' className='w-[20px] h-[20px]'/>
      </div>
