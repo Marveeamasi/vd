baa331fba66a7f9208b4cd34e13e836de93a50f6
Marvellous Amasi <102649743+Marveeamasi@users.noreply.github.com>
2025-04-08 15:53:36 +0100

    Add files via upload

---
+++ dummyData.js
@@ -0,0 +1,9 @@
+export const avatarVoicesDb = [
+    {name: 'zeeno', total:'1'},
+]
+
+export const elevenVoicesDb = [
+    {name: 'Jake'}, {name: 'Sarah'}, {name: 'John'}, {name: 'Jude'}, {name: 'Chioma'},
+    {name: 'Luke'}, {name: 'Peter'}, {name: 'Drake'}, {name: 'Rukie'}, {name: 'Chan'},
+    {name: 'Ben'}, {name: 'Martha'}, {name: 'Moke'}, {name: 'Ruel'}, {name: 'Matt'},
+]
