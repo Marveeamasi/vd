42fbb838cf4ff16c67aefd1e75dd2b0cd5a89c03
Marvellous Amasi <102649743+Marveeamasi@users.noreply.github.com>
2025-04-14 11:39:02 +0100

    Update PreviewSideBar.jsx

--- components/PreviewSideBar.jsx
+++ components/PreviewSideBar.jsx
@@ -14,10 +14,10 @@ const handleSelectClick = (name) => {
 }
 
   return (
-    <aside className={`bg-[#261148] w-fit h-screen pt-10 pl-5 max-xl:pl-0 pb-5 pr-0 flex flex-col gap-5 font-[500] transition-all duration-300 ease-in-out text-[16px] sticky left-0 top-0 z-30 max-xl:fixed max-xl:left-0 max-xl:top-0 max-xl:bg-transparent max-xl:backdrop-blur-sm`}>
-     <Image src={'/logo.png'} width={214} height={44} className='w-[214px] h-[44px] max-xl:hidden' alt='logo'/>
-     <div className='overflow-y-scroll max-xl:pt-[70px] overflow-x-hidden'>
-     <div className='bg-[#140926] max-xl:bg-[#140926c0] relative max-xl:backdrop-blur-sm max-xl:border max-xl:border-[#ffffff17] rounded-[8px] p-5 flex gap-5 flex-col w-[295px]'>
+    <aside className={`bg-[#261148] w-fit h-screen pt-10 pl-5 max-lg:pl-0 pb-5 pr-0 flex flex-col gap-5 font-[500] transition-all duration-300 ease-in-out text-[16px] sticky left-0 top-0 z-30 max-lg:fixed max-lg:left-0 max-lg:top-0 max-lg:bg-transparent max-lg:backdrop-blur-sm`}>
+     <Image src={'/logo.png'} width={214} height={44} className='w-[214px] h-[44px] max-lg:hidden' alt='logo'/>
+     <div className='overflow-y-scroll max-lg:pt-[70px] overflow-x-hidden'>
+     <div className='bg-[#140926] max-lg:bg-[#140926c0] relative max-lg:backdrop-blur-sm max-lg:border max-lg:border-[#ffffff17] rounded-[8px] p-5 flex gap-5 flex-col w-[295px]'>
      <PreviewSideBarSelect setBlurbg={setBlurbg} selectName={selectName} setSelectName={setSelectName} handleSelectClick={handleSelectClick} pos={'top-[110%]'} type={''} name={'Avatar'} options={['John Doe','Simon Phil', 'Ruda Mark', 'Jane Path']} image={'/user-circle.png'}/>
       <PreviewSideBarSelect setBlurbg={setBlurbg} selectName={selectName} setSelectName={setSelectName} handleSelectClick={handleSelectClick} pos={'top-[110%]'} type={''} name={'Camera'} options={['Face','Side Profile']} image={'/face-id.png'}/>
       <div className={`flex relative ${selectName==='Body Language'? 'z-50':'z-10'} items-center justify-between text-[12.51px] font-[700]`}>
